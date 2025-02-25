424376e0259da8e3bb203b8294fe2700bb9a6192
DeveloperTum <devlopertum>
2025-02-25 21:38:11 +0300

    additional html

--- basics.php
+++ basics.php
@@ -1,6 +1,15 @@
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport content="width=device-width, initial scale=1>
+    <title>Recap of PHP Programming</title>
+</head>
+<body>
 <?php
 // simple code to print something
 echo"Exprience a quick dive into Php Programming";
 
-
 ?>
+</body>
+</html>
